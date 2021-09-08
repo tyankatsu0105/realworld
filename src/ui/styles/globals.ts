4feeafe37f3874499ac71d9e7055499643f51616
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-09-08 21:38:52 +0900

    change how to import font

--- src/ui/styles/globals.ts
+++ src/ui/styles/globals.ts
@@ -1,9 +1,15 @@
 import { css } from '@linaria/core';
 
 export const globals = css`
+  @import url('https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap');
+
   :global() {
     html {
       font-family: 'Teko', sans-serif;
+    }
+
+    a {
+      cursor: pointer;
     }
   }
 `;
